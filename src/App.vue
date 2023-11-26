6cd670430ab4b298a41cc88c061aac958d2ae893
alxgalben <galbenalex2@gmail.com>
2023-11-26 23:52:47 +0200

    first preview

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,120 @@
-<template>
-  <div id="app">
+<script setup>
+import Playground from './components/Playground.vue';
 
+</script>
+
+<template>
+  <div v-if="!showSecondSearchBox" class="card search-box">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example" style="height: 40px;">
+          <option selected>Locatie de preluare</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+
+        <div class="predare-alta-locatie-check" style="margin-top: 5px;">
+          <input @click="showField = !showField" class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault" style="font-size: 13px; margin-left: 7px;">
+            Doresc sa predau masina in alta locatie
+          </label>
+        </div>
+
+        <select v-if="showField" class="form-select form-select-sm" aria-label=".form-select-sm example" style="height: 40px; margin-top: 5px;">
+          <option selected>Locatie de predare</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+
+      </li>
+      <li class="list-group-item"><Playground /></li>
+    </ul>
+    <button type="button" class="btn btn-warning btn-search">Search</button>
+  </div>
+
+  <div v-if="showSecondSearchBox" class="card search-box">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example" style="height: 40px;">
+          <option selected>Locatie de preluare</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </li>
+      <li class="list-group-item">
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example" style="height: 40px; margin-top: 5px;">
+          <option selected>Locatie de predare</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </li>
+    </ul>
+    <button type="button" class="btn btn-warning btn-search">Search</button>
+  </div>
+
+  <div class="privat">
+    <button @click="toggleSearchBoxes" type="button" class="btn btn-dark btn-transfer-privat">{{ buttonText }}</button>
   </div>
 </template>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
+@media only screen and (max-width: 900px) {
+    
+    .search-box {
+      width: 90%;
+      margin: 10px auto 0;
+      border-radius: 0;
+      text-align: center;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .form-select-sm:hover {
+      cursor: pointer;
+    }
+
+    .btn-search {
+      width: 95%;
+      border-radius: 0;
+      margin: 10px auto;
+    }
+
+    .privat {
+      width: 90%;
+      margin: 15px auto 0;
+      text-align: right;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      showField: false,
+      showSecondSearchBox: false,
+      buttonText: "Vreau transfer privat",
+    };
+  },
+  methods: {
+    toggleSearchBoxes() {
+      this.showField = false;
+      this.showSecondSearchBox = !this.showSecondSearchBox;
+      this.buttonText = this.showSecondSearchBox ? "Vreau inchiriere auto" : "Vreau transfer privat";
+    },
+  },
+};
+</script>
---
+++ src/components/Playground.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+      <Datepicker v-model="date" 
+      range multi-calendars 
+      :min-date="new Date()"
+      :max-date="maxDate" 
+      prevent-min-max-navigation
+      placeholder="Selecteaza data de preluare si predare"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import Datepicker from '@vuepic/vue-datepicker';
+  import '@vuepic/vue-datepicker/dist/main.css';
+  
+  const date = ref();
+  </script>
+  
